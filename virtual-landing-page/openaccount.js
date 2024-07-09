246917b50fd54a35928bb585b5dad9a70322fe90
tharoyng <tharo.yng@gmail.com>
2024-07-09 16:57:50 +0700

    add good morning

---
+++ virtual-landing-page/openaccount.js
@@ -0,0 +1,16 @@
+
+
+function updateGreeting() {
+    const greetingElement = document.getElementById('greeting-time');
+    const currentHour = new Date().getHours();
+    
+    if (currentHour < 12) {
+      greetingElement.textContent = 'Chào buổi sáng,';
+    } else if (currentHour < 18) {
+      greetingElement.textContent = 'Chào buổi trưa,';
+    } else {
+      greetingElement.textContent = 'Chào buổi tối,';
+    }
+  }
+
+  window.onload = updateGreeting;
